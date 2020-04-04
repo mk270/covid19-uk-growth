66c62f688560b0adcdf669b239ccf7bccb6e1e4a
Martin Keegan <martin@no.ucant.org>
2020-04-04 13:44:30 +0100

    use logging modeul

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ from sqlite3 import dbapi2 as sqlite
 import sqlite3
 import sys
 import datetime
+import logging
 
 DB = 'cached.db'
 SQL = """INSERT INTO cases_log (day, cases, tested) VALUES (?, ?, ?);"""
@@ -12,8 +13,8 @@ def save_update(day_formatted, count, tested):
         with db:
             db.execute(SQL, (day_formatted, count, tested))
     except sqlite3.IntegrityError:
-        print("Couldn't update DB")
-        print("This is probably because today's data not yet available")
+        logging.warn("Couldn't update DB")
+        logging.warn("This may be because today's data is not yet available")
         sys.exit(1)
 
 def already_done_today(today):
