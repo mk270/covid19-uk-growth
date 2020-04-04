d7cd714bf7a77945ffab4e9f0825526e5e5dac91
Martin Keegan <martin@no.ucant.org>
2020-04-04 09:11:04 +0100

    split database into own file

---
+++ database.py
@@ -0,0 +1,25 @@
+from sqlite3 import dbapi2 as sqlite
+import sqlite3
+import sys
+import datetime
+
+DB = 'cached.db'
+SQL = """INSERT INTO cases_log (day, cases) VALUES (?, ?);"""
+
+def save_update(day_formatted, count):
+    db = sqlite.connect(DB)
+    try:
+        with db:
+            db.execute(SQL, (day_formatted, count))
+    except sqlite3.IntegrityError:
+        print("Couldn't update DB")
+        print("This is probably because today's data not yet available")
+        sys.exit(1)
+
+def already_done_today(today):
+    db = sqlite.connect(DB)
+    sql = """SELECT day from cases_log WHERE day = ?;"""
+    with db:
+        results = db.execute(sql, (today,))
+        rowcount = len(results.fetchall())
+        return rowcount > 0
